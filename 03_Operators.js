fc36c7ea9662da63cd2840ddd694a747d2ae601d
Dilara <dcavus@gmail.com>
2023-05-02 14:29:59 -0700

    add: logical

---
+++ 03_Operators.js
@@ -0,0 +1,18 @@
+// Order of operators same as Java
+console.log(4+4*(-1)-8/2);
+// assignment operators
+let x=5;
+x+=5;
+console.log(x);
+// comparison : similar to Java 
+let one=1;
+let one_more=1;
+let one_string='1';
+let two_string='2';
+console.log(one==one_more);
+console.log(one==one_string); // == value equality
+console.log(one_string===two_string); // === data type and value equality
+
+// logical operators same like Java only limited to && ||
+
+console.log((5<2)&&(2<5));
