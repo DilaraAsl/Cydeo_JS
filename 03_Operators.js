99db21c323f3cc4160ca19e2fe68ae29a2553f28
Dilara <dcavus@gmail.com>
2023-05-02 14:39:46 -0700

    add:if statement

--- 03_Operators.js
+++ 03_Operators.js
@@ -16,3 +16,12 @@ console.log(one_string===two_string); // === data type and value equality
 // logical operators same like Java only limited to && ||
 
 console.log((5<2)&&(2<5));
+
+let num1=100;
+let num2=10;
+if(num1>num2){
+    console.log('number 1 is bigger');
+}
+else{
+    console.log('number 2 is bigger');
+}
