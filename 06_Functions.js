abd5280b481fb8ff887e1bbee0b69b75f80104aa
Dilara <dcavus@gmail.com>
2023-05-03 09:20:48 -0700

    functions

---
+++ 06_Functions.js
@@ -0,0 +1,12 @@
+// no access modifier, no method overloading
+// non return type
+function add(a,b) { 
+    console.log("Total is : "+(a+b));
+    
+}
+// return type 
+add(10,20);
+function name(a,b) {
+    return (a*b);
+}
+console.log(multiply(10,20));
