ce70e890b5d528d6c7d6ccc95701fdc6c4df0282
Dilara <dcavus@gmail.com>
2023-05-02 17:04:28 -0700

    git commit -m "commit"

--- 04_Loops.js
+++ 04_Loops.js
@@ -1,11 +1,11 @@
 // loops same as Java : for , while , do while
-for(let i=0; i<5; i++){
+for(let i=0; i<5; i++){ // let local scope
     console.log(i);
 }
 
 // console.log(i); will get Reference error because of variable scope
-
-for(var j=0; j<5; j++){
+// old type of declaring variables ES6
+for(var j=0; j<5; j++){ //global function scope
     console.log(j);
 }
 console.log(j); // prints 5 var j does not stay inside the scope of for loop 
