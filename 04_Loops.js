1ca596fcd81a5fc0f64898b52719edbe25ab7686
Dilara <dcavus@gmail.com>
2023-05-02 18:20:05 -0700

    do while and while loops added

--- 04_Loops.js
+++ 04_Loops.js
@@ -9,3 +9,15 @@ for(var j=0; j<5; j++){ //global function scope
     console.log(j);
 }
 console.log(j); // prints 5 var j does not stay inside the scope of for loop 
+
+let n1=0;
+while (n1<3) {
+    console.log(n1);
+    n1++;
+    
+}
+let n2=0;
+do{
+    console.log(n2);
+    n2++;
+}while(n2<3);
